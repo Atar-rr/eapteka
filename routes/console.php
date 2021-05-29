937ae5ef7b1fac5a1292df0a638d67e4e36fed97
Сергей Диденко <s.didenko@rabota.ru>
2021-05-29 17:01:26 +0300

    Миграции + поиск

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Drugs;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +18,31 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('create_drugs', function () {
+    $file = simplexml_load_string(file_get_contents(__DIR__ . '/../data.xml'));
+    $json = json_encode($file);
+    $data = current(json_decode($json, true));
+
+    foreach ($data as $item) {
+        if (!isset($item['as_name_rus'])) {
+            continue;
+        }
+        $active = explode('+', $item['as_name_rus']);
+        $drug = new Drugs();
+        $drug->packing_id = isset($item['packing_id']) ? (int)$item['packing_id'] : null;
+        $drug->desc_id = isset($item['desc_id']) && is_string($item['desc_id']) ? (int)$item['desc_id'] : null;
+        $drug->prep_id = isset($item['prep_id']) ? (int)$item['prep_id'] : null;
+        $drug->as_id = isset($item['as_id']) ? (int)$item['as_id'] : null;
+        $drug->dosage_form_id = isset($item['dosage_form_id']) ? (int)$item['dosage_form_id'] : null;
+        $drug->dosage_form_full_name = $item['dosage_form_full_name'] ?? null;
+        $drug->as_name_rus = mb_strtolower($item['as_name_rus']);
+        $drug->as_name_primary = isset($active[0]) ? mb_strtolower(trim($active[0])) : null;
+        $drug->as_name_secondary = isset($active[1]) ? mb_strtolower(trim($active[1])) : null;
+        $drug->amount = $item['amount'] ?? null;
+        $drug->prep_short = mb_strtolower($item['prep_short']);
+        $drug->prep_full = mb_strtolower($item['prep_full']);
+
+        $drug->save();
+    }
+});
